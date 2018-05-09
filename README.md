e140513ef7d47b29c758e3294efaa01cf558d345
Daniel Kwon <daniel.kwon@nexstreaming.com>
2018-05-09 17:36:11 +0900

    Created database and added test ldap server.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # node-js-login-session
 This is the test for the login session of node.js.
+
+## Before start..
+1. If you don\`t have any mysql database, go to your preferences and open MySQL Server pererence. Click on `Start MySQL Server`.
+	-  To make a Databse, do the following
+		~~~
+		bash $ mysql -u root -h 127.0.0.1 -p1234 # enter the temporary 
+		~~~
+
+	- If you want to change the password of root user, please refer to this.
+		~~~
+		mysql> ALTER USER 'root'@'localhost' IDENTIFIED BY 'new-password';
+		~~~
+
+	- Create Databse
+		~~~
+		mysql> CREATE DATABASE node_js_login_session character set UTF8 collate utf8_bin;
+		~~~
+
+	- Use Database
+		~~~
+		mysql> USE node_js_login_session;
+		~~~
+
+2. Copy & paste below DDLs on the your database command window.
+	- Create User Table.
+		~~~
+		CREATE TABLE `user` ( `no` int(10) NOT NULL AUTO_INCREMENT, `id` varchar(30) DEFAULT NULL, `passwd` varchar(80) NOT NULL, `auth_codes` varchar(20) NOT NULL, `mail` varchar(80) NOT NULL, `regist_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`no`)) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+		~~~
+	- Insert few Users.
+		~~~
+		INSERT INTO user(id, passwd, auth_codes, mail) VALUES('daniel.kwon', '1234', 'SA,SM,AM,TA,SE', 'daniel.kwon@test.email');
+		~~~
+		~~~
+		INSERT INTO user(id, passwd, auth_codes, mail) VALUES('derrick.kang', '1234', 'SE', 'derrick.kang@test.email');
+		~~~
+
+## Run
+~~~
+node ./bin/www
+~~~
--- modules/database/ldap.js
+++ modules/database/ldap.js
@@ -1,38 +1,33 @@
 var ldap = require('ldapjs');
 
 var client = ldap.createClient({
-    url: 'ldap://192.1.1.1:389'
+    url: 'ldap://ldap.forumsys.com  :389'
 });
 
 function setSearchOption(username) {
-    return { filter: '(samaccountname='+username+')', scope: 'sub', attributes: ['sAMAccountName','mail'] };
+    return { filter: "(uid=tesla)", scope: 'sub', attributes: ['sn','mail'] };
 }
 
 module.exports = {
     isAuthenticatedUser : function isAuthenticatedUser(req, username, password, ret, done) {
         try {
-            client.bind(username+'@nexstreaming.com', password, function (error) {
+            client.bind('cn=read-only-admin,dc=example,dc=com', 'password', function (error) {
                 if (error) {
                     return done(null, false, req.flash('loginMessage', 'Invalid ID or PW. - ' + error.message));
                 }
+                var option = setSearchOption(username);
+                client.search('dc=example,dc=com', option, function (error, res) {
+                    res.on('searchEntry', function(entry) {
+                        if(entry.object){
+                            console.log(JSON.stringify(entry.object));
+                            ret.username = entry.object['sn'];
+                            ret.email = entry.object['mail'];
+                        }
+                        return done(null,ret);
+                    });
 
-                client.bind('ldapadmin1@nexstreaming.com', 'goAuthLdap09!', function (error) {
-                    if (error) {
+                    res.on('error', function(error) {
                         return done(null, false, req.flash('loginMessage', 'Invalid ID or PW. - ' + error.message));
-                    }
-                    var option = setSearchOption(username);
-                    client.search('OU=NexStreaming,DC=nexstreaming,DC=com', option, function (error, res) {
-                        res.on('searchEntry', function(entry) {
-                            if(entry.object){
-                                ret.username = entry.object['sAMAccountName'];
-                                ret.email = entry.object['mail'];
-                            }
-                            return done(null,ret);
-                        });
-
-                        res.on('error', function(error) {
-                            return done(null, false, req.flash('loginMessage', 'Invalid ID or PW. - ' + error.message));
-                        });
                     });
                 });
             });
